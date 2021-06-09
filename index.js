864950e679c3d848f7237d7f3fa5ab83992f8585
Aric McAlister <aricmca@gmail.com>
2021-06-08 19:26:52 -0500

    commit tree b09a865f61c809ac6904565b9a51871243214d9d

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return "This one is on me!"
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const isCity = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return isCity;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  console.log(tip)
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+
 }
